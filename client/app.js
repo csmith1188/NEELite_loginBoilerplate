a828c74d33feae4f2a5fdba78d4706dc19563b16
APlitt24235 <Alexa24235@live.ytech.edu>
2023-09-15 11:17:35 -0400

    Added Comments

--- client/app.js
+++ client/app.js
@@ -2,12 +2,10 @@
 const express = require('express')
 const session = require('express-session')
 const sqlite3 = require('sqlite3').verbose()
-const bcrypt = require('bcrypt')
-const cookieParser = require("cookie-parser");
 const jwt = require('jsonwebtoken');
 
 
-// Database Setup
+// Database Setup (not used here, however for independent projects using this API this would be useful for storing user data for that project)
 const database = new sqlite3.Database('./client/database.db', sqlite3.OPEN_READWRITE)
 
 
@@ -16,6 +14,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
+/* Sets view engine to './client/views' for testing purposes. In independent projects this is not needed, but it is needed here due to both the client
+  and server being stored in the same directory.*/
 app.set('views', './client/views');
 app.use(session({
   secret: 'D$jtDD_}g#T+vg^%}qpi~+2BCs=R!`}O',
@@ -24,11 +24,19 @@ app.use(session({
 }))
 
 // Setup Constants
+
+//Saves port of your choosing.
 const port = 4000
+/* Auth URL is wherever you'd like to redirect for verification/authentication. For example, when creating a plugin (which is most recommended for usage of this API), 
+   you'd want to set AUTH_URL to whatever your plugin is for in order for it to authenticate the user in both the plugin and server. */
 const AUTH_URL = 'http://127.0.0.1:3000/oauth'
+/* This is whatever your login page is set to, in this case, it is simply '/login'. */
 const THIS_URL = 'http://127.0.0.1:4000/login'
 
 // Functions
+
+/* This authenticates the user and checks to see if the user is logged in. If so, it will continue the process like usual, if not, it will redirect you to the login
+   page. */
 function isAuthenticated(request, response, next) {
   if (request.session.user) next()
   else response.redirect('/login')
@@ -36,6 +44,9 @@ function isAuthenticated(request, response, next) {
 
 
 // Webpages
+
+/* This is the homepage. It uses our function isAuthenticated to check for the user. When that succeeds, it tried to render your index/home page (depending on
+   what you decide to name it) and carries over the 'request.session.user' data as the 'user' variable. Where this data comes from will be covered in '/login'. */
 app.get('/', isAuthenticated, (request, response) => {
   try {
     response.render('index.ejs', { user: request.session.user })
@@ -45,40 +56,37 @@ app.get('/', isAuthenticated, (request, response) => {
   }
 })
 
-/*app.get('/login', (request, response) => {
-  if (request.cookies.token) {
-    response.redirect(AUTH_URL + `?redirectURL=${THIS_URL}&token=${request.cookies.token}`)
-  }
-  else if (request.session.user) {
-    request.session.user = username
-    request.session.save()
-    response.redirect('/')
-  } else response.redirect(AUTH_URL + `?redirectURL=${THIS_URL}`);
-}) */
-
+/* This is what initially happens when you try to login. */
 app.get('/login', (req, res) => {
+  /* First, it checks if there is the query parameter of 'token'. This is used to check if there is a token present, which is carried over from the server side
+   as said query parameter. */
   if (req.query.token) {
     console.log(req.query.token);
+    /* If there is a token parameter present, then it decodes the token and stores it into a temporary variable. This is because, every time the user reconnects to the server, 
+    there will be a new token generated. */
     let tokenData = jwt.decode(req.query.token);
+    /* It then saves the entirety of the token data into a cookie, with only the username data being stored in another. */
     req.session.token = tokenData;
     req.session.user = tokenData.username;
+    /* From there, it redirects back to your homepage. */
     res.redirect('/');
+    /* If there is not token, it redirects you to the server authentication page, with the query perameter of 'redirectURL' being given the THIS_URL variable, 
+    which should be your login page (or your page of choice).  */
   } else {
     res.redirect(AUTH_URL + `?redirectURL=${THIS_URL}`)
   };
 })
 
+// This logsout the user by overwriting the user cookie, which contains the username. It then saves the session and reloads it. It then redirects the user to the login page. 
 app.get('/logout', (request, response) => {
   request.session.user = null
   request.session.save((error) => {
     if (error) throw error
-    request.session.regenerate((error) => {
-      if (error) throw error
       response.redirect('/login')
-    })
   })
 })
 
+// This starts the client on whatever port you choose.
 app.listen(port, (err) => {
   if (err) {
     console.error(err)
--- server/app.js
+++ server/app.js
@@ -16,6 +16,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
+/* Sets view engine to './server/views' for testing purposes. This will not be present normally, as the client typically connects to a server not present
+in the same file directory. For example, the client, 172.168.7.5:3000, would connect to the server, 172.168.9.6:3000.*/
 app.set('views', './server/views');
 app.use(session({
   secret: 'D$jtDD_}g#T+vg^%}qpi~+2BCs=R!`}O',
@@ -27,21 +29,18 @@ app.use(session({
 const port = 3000
 
 // Functions
+
+/* This authenticates the user and checks to see if the user is logged in. If so, it will continue the process like usual, if not, it will redirect you to the login
+   page. */
 function isAuthenticated(request, response, next) {
   if (request.session.user) next()
   else response.redirect('/login')
 }
 
-/*const tokenGenerator = (issuer, lifeSpan, secret) => {
-  var payload = {
-    "iss": issuer,
-    "exp": (new Date() / 1000) + lifeSpan
-  };
-  var token = jwt.sign(payload, secret, algorithm = 'RS256');
-  return token;
-}; */
-
 // Webpages
+
+/* This is the homepage. It uses our function isAuthenticated to check for the user. When that succeeds, it tried to render your index/home page (depending on
+   what you decide to name it) and carries over the 'request.session.user' data as the 'user' variable. Where this data comes from will be covered in '/login'. */
 app.get('/', isAuthenticated, (request, response) => {
   try {
     response.render('index.ejs', { user: request.session.user })
@@ -51,32 +50,40 @@ app.get('/', isAuthenticated, (request, response) => {
   }
 })
 
+// This is the login page, which renders your login page.
 app.get('/login', (request, response) => {
   response.render('login.ejs')
 })
 
+/* This is what happens after login data is submitted.*/
 app.post('/login', (request, response) => {
+  // It saves the username and password collected from the data submitted.
   const { username, password } = request.body
-  request.session.regenerate((error) => {
-    if (error) console.log(error)
+  // If both a username and password are saved, then it will grab data from the database where the username matches.
     if (username && password) {
       database.get(`SELECT * FROM users Where username = ?`, [username], (error, results) => {
         if (error) console.log(error)
         if (results) {
+          // Using these results, it will save the password present in the database to a variable.
           let databasePassword = results.password
+          // It then compares the password submitted and the database password to see if they match.
           bcrypt.compare(password, databasePassword, (error, isMatch) => {
             if (isMatch) {
               if (error) console.log(error)
+              // If they do, save the username to the user cookie and redirect to the home page.
               request.session.user = username
               response.redirect('/')
+            // If the passwords do not match, redirect to the '/login' page again to resubmit data.
             } else response.redirect('/login')
           })
+        // If there are no results in the database, redirect to the '/login' page again to resubmit data.
         } else response.redirect('/login')
       })
+    // If there is no username, no password, or neither present in the submitted data, then redirect to the '/login' page again to resubmit data.
     } else response.redirect('/login')
-  })
 })
 
+// This is what happens when the user tries to sign up. It simply renders the 'signup.ejs' page.
 app.get('/signup', (request, response) => {
   try {
     response.render('signup.ejs')
@@ -86,43 +93,51 @@ app.get('/signup', (request, response) => {
   }
 })
 
+/* This is what happens after the user submits signup data.*/
 app.post('/signup', (request, response) => {
+  // It saves the data, including the confirmed password.
   const { username, password, confirmPassword } = request.body
-  request.session.regenerate((error) => {
-    if (error) console.log(error)
+  // If all of the data is collected, it tries to get data from the database based on the username.
     if (username && password && confirmPassword) {
       database.get(`SELECT * FROM users Where username = ?`, [username], (error, results) => {
         if (error) console.log(error)
+        //If there are no results returned, meaning the username doesn't exist, and if the password and the confirmed password match, then it encrypts the password.
         if (!results) {
           if (password == confirmPassword) {
             bcrypt.hash(password, 10, (error, hashedPassword) => {
               if (error) console.log(error);
+              // Then it creates a secret based on 512 random bytes collected and saves it after converting it to a hex string.
               let secret = crypto.randomBytes(512);
               secret = secret.toString('hex');
+              // Afterwards, it inserts the data into the database, creating a new user.
               database.get(`INSERT INTO users (username, password, secret ) VALUES (?, ?, ?)`, [username, hashedPassword, secret], (error) => {
                 if (error) console.log(error)
+                // It also creates a user cookie which the username is stored into, and then it redirects to the homepage.
                 request.session.user = username
                 response.redirect('/')
               })
             })
+          // If the passwords don't match, redirect to the sign up page to re-enter data.
           } else response.redirect('/signup')
+        // If there are results in the database, then redirect to the sign up page to re-enter results.
         } else response.redirect('/signup')
       })
+    // If there is no username, password, and/or confirmed password, then redirect to the sign up page to re-enter data.
     } else response.redirect('/signup')
-  })
 })
 
+// This is what happens when the user tries to log out.  
 app.get('/logout', (request, response) => {
+  // It resets the session.user (or the user "cookie", as referred to earlier), and then it saves it.
   request.session.user = null
   request.session.save((error) => {
     if (error) console.log(error)
-    request.session.regenerate((error) => {
-      if (error) console.log(error)
+    // After the new user cookie is saved, it redirects to the login page.
       response.redirect('/login')
-    })
   })
 })
 
+//This is what happens when the user tries to change their password. It simply renders the changePassword page.
 app.get('/changePassword', (request, response) => {
   try {
     response.render('changePassword.ejs')
@@ -132,37 +147,50 @@ app.get('/changePassword', (request, response) => {
   }
 })
 
+/* This is what happens when the user submits the changed password data.*/
 app.post('/changePassword', (request, response) => {
+  // It saves the current password, the new password, and the confirmed new password based on the submitted data.
   const { currentPassword, newPassword, confirmNewPassword } = request.body
+  // It saves the user cookie data to a username variable.
   const username = request.session.user
+  // It then get data from the database based on that username.
   database.get(`SELECT password FROM users Where username = ?`, [username], (error, results) => {
     if (error) console.log(error)
+    // If there are results, then it compares the current password that was submitted to the password present in the database.
     if (results) {
       bcrypt.compare(currentPassword, results.password, (error, isMatch) => {
         if (error) console.log(error)
+        // If it matches and the new password is the same as the confirmed new password, then it encrypts the new password and saves it to the database.
         if (isMatch && newPassword == confirmNewPassword) {
           bcrypt.hash(newPassword, 10, (error, hashedPassword) => {
             if (error) console.log(error)
             database.get('UPDATE users SET password = ? WHERE username = ?', [hashedPassword, username], (error, results) => {
               if (error) console.log(error)
+              // Then it redirects the user to the logout page.
               response.redirect('/logout')
             })
           })
+        // If the passwords don't match in either way, then it redirects the user to the home screen.
         } else response.redirect('/')
       })
+    // If there are no results given from the database, then it redirects the user to the home screen.
     } else response.redirect('/')
   })
 })
 
-
+/* This is what happens when the user tries to delete their account.*/
 app.get('/deleteAccount', (request, response) => {
+  // It saves the user cookie to the username variable, then deletes the user in the database where the username matches.
   username = request.session.user
   database.get('DELETE FROM users WHERE username = ?', [username], (error, results) => {
     if (error) console.log(error)
+    // Then it redirects the user to the logout page.
     response.redirect('/logout')
   })
 })
 
+/* This is what happens when the server tries to authenticate the user. It saves the redirectURL query parameter to a variable, andsends the redirectURL to the oauth page as
+a variable. */
 app.get('/oauth', (request, response) => {
   let redirectURL = request.query.redirectURL
   response.render('oauth.ejs', {
@@ -170,14 +198,15 @@ app.get('/oauth', (request, response) => {
   });
 })
 
+/* This is what happens after the user submits their authentication data. It saves the username, password, and the redirectURL that is submitted. If there is a username and
+password submitted, then it gets results from the database that match the username. If there are results returned, it saves the database password to a variable. It then compares
+the submitted password to the database password. */
 app.post('/oauth', (request, response) => {
   const {
     username,
     password,
     redirectURL
   } = request.body
-  request.session.regenerate((error) => {
-    if (error) console.log(error)
     if (username && password) {
       database.get(`SELECT * FROM users WHERE username = ?`, [username], (error, results) => {
         if (error) console.log(error)
@@ -196,7 +225,6 @@ app.post('/oauth', (request, response) => {
         } else response.redirect(`/oauth?redirectURL=${redirectURL}`)
       })
     } else response.redirect(`/oauth?redirectURL=${redirectURL}`)
-  })
 })
 
 
